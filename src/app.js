1853cfb2b57e97579cf8ddb0c27770aec3fcd3ab
Adedokun. Daud <daud.adedokun@gwl.ca>
2020-03-20 13:06:39 -0500

    weather app setup for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const path = require('path')
 const hbs = require('hbs')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 const partialsPath = path.join(__dirname, '../templates/partials')
@@ -54,6 +55,6 @@ app.get('/weather', (req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log('Server is listening on port 3000')
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
 })
